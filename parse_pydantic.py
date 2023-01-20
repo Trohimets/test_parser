a26f153fc061ddd419e63bd46f50d35a1d9795fe
Константин Трохимец <katrohimets@mail.ru>
2023-01-21 00:15:39 +0300

    add result data

--- parse_pydantic.py
+++ parse_pydantic.py
@@ -22,7 +22,7 @@ class Salary(BaseModel):
     gross: bool
 
 
-class Contacts(BaseModel):
+class Incoming_contacts(BaseModel):
     full_name: str = Field(alias='fullName')
     phone: str
     email: str
@@ -34,10 +34,58 @@ class Incoming_data(BaseModel):
     address: Address
     name: str
     salary: Salary
-    contacts: Contacts
+    contacts: Incoming_contacts
 
+
+class Phone(BaseModel):
+    city: str
+    country: str
+    number: str
+
+
+class Result_contacts(BaseModel):
+    email: str
+    name: str
+    phone: Phone
+
+
+class Coordinates(BaseModel):
+    latitude: float
+    longitude: float
+
+
+class Experience(BaseModel):
+    id: str
+
+
+class Salary_range(BaseModel):
+    till: int = Field(alias='from')
+    to: int
+
+
+class Schedule(BaseModel):
+    id: str
+
+class Result_data(BaseModel):
+    address: str
+    allow_messages: bool
+    billing_type: str
+    business_area: int
+    result_contacts: Result_contacts
+    coordinates: Coordinates
+    description: str
+    experience: Experience
+    html_tags: bool
+    image_url: str
+    name: str
+    salary: int
+    salary_range: Salary_range
+    schedule: Schedule
  
+
+
 incoming_data = Incoming_data.parse_raw(data)
+result_data = Result_data.
 print(incoming_data)
 
 
